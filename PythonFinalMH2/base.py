1c472ad7fb9784fddcda31405c20762431b0c6f3
mwhossain <59899816+mwhossain@users.noreply.github.com>
2020-03-14 11:50:33 -0700

    Add files via upload

---
+++ PythonFinalMH2/base.py
@@ -0,0 +1,21 @@
+def hello():	
+	return "Hello WORLD!"
+
+class salad():
+
+	def write():
+		#should receive a path
+		#should receive a list of salad items
+		#should receive a list of number of salad items
+		#should write a file with item.00.salad
+		#
+	#input path, [tomato, lettuce], [2,3]
+	#xxx/tomato_00.salad
+	#xxx/tomato_01.salad
+		return
+	def read ():
+	
+		#read the .salad files
+		#return a dictionary with th salad items and how many there are
+		
+##
